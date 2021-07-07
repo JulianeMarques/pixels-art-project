9030adc05b466668039121a0129687299e84c0a3
Juliane Marques <mqs.juliane@gmail.com>
2021-07-07 19:32:54 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -10,33 +10,32 @@ function creatingPixelBoardCollumns(row, colunas) {
 }
 
 function creatingPixelBoardRows(linhas) {
-    for (let row = 1; row <= linhas; row += 1) {
-      const divRow = document.createElement('div');
-      divRow.classList.add('tr');
-      pixelBoard.appendChild(divRow);
-      creatingPixelBoardCollumns(divRow, linhas);
-    }
+  for (let row = 1; row <= linhas; row += 1) {
+    const divRow = document.createElement('div');
+    divRow.classList.add('tr');
+    pixelBoard.appendChild(divRow);
+    creatingPixelBoardCollumns(divRow, linhas);
   }
+}
 
+creatingPixelBoardRows(5);
 
-  creatingPixelBoardRows(5);
+const paletteColors = document.querySelectorAll('.color');
 
-  const paletteColors = document.querySelectorAll('.color');
-  
-  function addingEventListenersToPalette(elements) {
-    for (let index = 0; index < elements.length; index += 1) {
-      elements[index].addEventListener('click', (event) => {
-        for (index = 0; index < elements.length; index += 1) {
-          elements[index].classList.remove('selected');
-        }
-        event.target.classList.toggle('selected');
-      });
-    }
+function addingEventListenersToPalette(elements) {
+  for (let index = 0; index < elements.length; index += 1) {
+    elements[index].addEventListener('click', (event) => {
+      for (index = 0; index < elements.length; index += 1) {
+        elements[index].classList.remove('selected');
+      }
+      event.target.classList.toggle('selected');
+    });
   }
-  
-  addingEventListenersToPalette(paletteColors);
+}
 
-  let pixels = document.querySelectorAll('.pixel');
+addingEventListenersToPalette(paletteColors);
+
+let pixels = document.querySelectorAll('.pixel');
 
 function addingEventListenersToPixels(elements) {
   for (let index = 0; index < elements.length; index += 1) {
@@ -50,3 +49,51 @@ function addingEventListenersToPixels(elements) {
 }
 
 addingEventListenersToPixels(pixels);
+
+function clearingTheBoard() {
+  const button = document.querySelector('#clear-board');
+  button.addEventListener('click', () => {
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.backgroundColor = 'white';
+    }
+  });
+}
+
+clearingTheBoard();
+
+const inputButton = document.querySelector('#generate-board');
+const input = document.querySelector('#board-size');
+
+function changingBoardSize() {
+  inputButton.addEventListener('click', () => {
+    if (input.value === '') return alert('Board inválido!');
+    const divsRowsPixels = pixelBoard.querySelectorAll('.tr');
+    for (let index = 0; index < divsRowsPixels.length; index += 1) {
+      pixelBoard.removeChild(divsRowsPixels[index]);
+    }
+    if (input.value < 5) input.value = 5;
+    if (input.value > 50) input.value = 50;
+    creatingPixelBoardRows(input.value);
+    pixels = document.querySelectorAll('.pixel');
+    addingEventListenersToPixels(pixels);
+  });
+}
+
+changingBoardSize();
+
+window.onload = () => {
+  const blackColor = document.querySelector('#first-color');
+  blackColor.classList.add('selected');
+  // Ideia retirada de https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
+  function randomColors() {
+    const red = Math.random() * 255;
+    const green = Math.random() * 255;
+    const blue = Math.random() * 255;
+
+    return `rgb(${red},${green},${blue})`;
+  }
+
+  for (let color = 1; color < paletteColors.length; color += 1) {
+    paletteColors[color].style.backgroundColor = randomColors();
+  }
+};
